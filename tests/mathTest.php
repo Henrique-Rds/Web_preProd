ebc0df9a4878721d72abf7b1a6373ef5e6aec3cb
Henrique Rodrigues <henrique.rodrigues@etu.iut-tlse3.fr>
2022-04-06 16:47:10 +0200

    Update mathTest.php

--- tests/mathTest.php
+++ tests/mathTest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 class MathTest extends TestCase
 {
     public function testDouble(){
-        $this->assertEquals(4, \tuto\Math::double(2));
+        $this->assertEquals(4, Math::double(2));
 
     }
 }
